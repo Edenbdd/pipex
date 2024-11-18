fb7e9d7e3662709d2cb89d923634bf302acdd4b6
Eden Bertrand <aubertra@made-f0Br7s14.clusters.42paris.fr>
2024-11-18 11:40:55 +0100

    heredoc works everything needs to be cleaned

--- srcs_bonus/processes_bonus.c
+++ srcs_bonus/processes_bonus.c
@@ -6,7 +6,7 @@
 /*   By: aubertra <aubertra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/09 11:48:25 by aubertra          #+#    #+#             */
-/*   Updated: 2024/11/18 11:10:37 by aubertra         ###   ########.fr       */
+/*   Updated: 2024/11/18 11:39:25 by aubertra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,14 +50,29 @@ int heredoc_exec(char **argv, char **env, t_err *err)
     char *current_line;
     int id;
     int id2;
+	int	id_write;
+	char	buffer[150];
 
     check_access(NULL, argv[5], err);
     err->previous_fd = open("heredoc_tmp", O_RDWR | O_CREAT | O_TRUNC, 0644);
     error_exit(err->previous_fd, -1, error_msg(err, "creation of heredoc_tmp failed "), err);
-    current_line = get_next_line(STDIN_FILENO, argv[2]);
-    write(err->previous_fd, current_line, ft_strlen(current_line));
-    err->cmds = get_cmds(argv, 6, err, 3);
+	id_write = fork();
+	if (id_write == 0)
+	{
+    	current_line = get_next_line(STDIN_FILENO, argv[2]);
+		write(err->previous_fd, current_line, ft_strlen(current_line));
+		free_close(err);
+		free(current_line);
+		exit(0);
+	}
+	wait(NULL);
+	close(err->previous_fd);
+    err->previous_fd = open("heredoc_tmp", O_RDWR | O_CREAT, 0644);
     error_exit(pipe(err->fd), -1, error_msg(err, "pipe failed "), err);
+	// int n = read(err->previous_fd, buffer, 149);
+	// buffer[n] = 0;
+	printf("buffer %s\n", buffer);
+    err->cmds = get_cmds(argv, 6, err, 3);
     id = fork();
     error_exit(id, -1, error_msg(err, "fork failed "), err);
     if (id == 0)
@@ -71,12 +86,11 @@ int heredoc_exec(char **argv, char **env, t_err *err)
 	}
 	free_close(err);
 	waiting(id2);
-    if (unlink("heredoc_tmp") == -1)
-	{
-		printf("error de unlink\n");
-		return (-1);
-	}
-	free(current_line);
+    // if (unlink("heredoc_tmp") == -1)
+	// {
+	// 	printf("error de unlink\n");
+	// 	return (-1);
+	// }
 	return (0);
 }
 
